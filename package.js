3a3c4d47f2ae67efeb68b14eb20711964e4193c3
Christian Grlica <christian@heavenseven.net>
2015-12-22 20:00:05 +0100

    first commit

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'heaven7:wsl-permissions',
+  version: '0.0.1',
+  summary: 'Permissions for wsl packages',
+  git: 'https://github.com/heaven7/wsl-permissions.git',
+  documentation: 'README.md'
+});
+
+var both = ['client','server'],
+    packages = [
+        'heaven7:wsl-core@0.0.2',
+        'alanning:roles@1.2.14'
+    ];
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2');
+    api.use(packages, both);
+    api.imply(packages);
+
+    api.addFiles([
+        'lib/server/methods.js',
+        'lib/server/permissions.js'
+    ], 'server');
+
+});
